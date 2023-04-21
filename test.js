54827223b51dd035b699012fb1f6d2b5788114be
JayDaMan53 <118361901+JayDaMan53@users.noreply.github.com>
2023-04-20 20:58:00 -0400

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+const list1 = ['item1', 'item2', 'item3'];
+const list2 = ['itemA', 'itemB', 'itemC'];
+const list3 = ['itemX', 'itemY', 'itemZ'];
+
+window.myLists = {
+  list1: list1,
+  list2: list2,
+  list3: list3
+};
